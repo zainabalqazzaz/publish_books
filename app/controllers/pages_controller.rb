9139dc5ae537aab02659745d798a159a1fd66fa6
zainab <zainabalqazzaz@gmail.com>
2024-12-02 13:51:01 -0500

    change the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @a="this is my first application"
+   @books=Book.where("created_at >= ?", 24.hours.ago)
   end
 
   def about 
